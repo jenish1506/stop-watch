bed584088c7262b09b0e0c44089322f87a8b37d0
jenish <jk.ugaminfotech@gmail.com>
2022-07-29 18:00:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import { useState } from 'react'
+
+let adjustInterval = undefined;
+
+function App() {
+  const [watch, setWatch] = useState(0)
+  const [started, setStarted] = useState(false)
+
+  const startwatch = () => {
+    adjustInterval = setInterval(() => {
+      setWatch((x) => x + 1)
+    }, 1000)
+    setStarted(true)
+  }
+  const stopwatch = () => {
+    clearInterval(adjustInterval)
+    setStarted(false)
+  }
+  const resetwatch = () => {
+    setWatch(0)
+    clearInterval(adjustInterval)
+    setStarted(false)
+  }
+
+  return (
+    <div className="App">
+      <h1>start Stop watch</h1>
+      <h1>{watch}</h1>
+      <button className='start' disabled={started} onClick={startwatch}>Start</button>
+      <button className='stop' onClick={stopwatch}>Stop</button>
+      <button className='reset' onClick={resetwatch}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
